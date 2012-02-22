21012f674b7f83b1aa436eab31b8354831e2cdf5
mizon <mizon9@gmail.com>
2012-02-22 23:00:43 +0900

    new functions

--- RicEvents/Database.hs
+++ RicEvents/Database.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module RicEvents.Database
-  ( Attendee(..), AttendeeDB(..)
+  ( Attendee(..), AttendeeDB(..), put, get, all
+  , loadDB, saveDB
   ) where
 
 import Data.Aeson ((.=), (.:))
@@ -11,9 +12,12 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Attoparsec as At
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import System.IO
 import Control.Applicative
 import qualified System.FilePath as F
+
+import Prelude hiding (all)
 
 data Attendee
   = Attendee
@@ -45,6 +49,15 @@ data AttendeeDB
     , dbPath :: F.FilePath
     }
 
+put :: Attendee -> AttendeeDB -> AttendeeDB
+put a (db@AttendeeDB {dbAttendees = as}) = db {dbAttendees = M.insert (aName a) a as}
+
+get :: String -> AttendeeDB -> Maybe Attendee
+get key (db@AttendeeDB {dbAttendees = as}) = M.lookup key as
+
+all :: AttendeeDB -> [Attendee]
+all = M.elems . dbAttendees
+
 loadDB :: F.FilePath -> IO AttendeeDB
 loadDB path = do
   m <- toMap `fmap` BS.readFile path
@@ -59,3 +72,6 @@ loadDB path = do
     attendees' c = case At.parseOnly A.json c of
                     Right v -> AT.parse A.parseJSON v
                     Left e  -> A.Error e
+
+saveDB :: AttendeeDB -> IO ()
+saveDB db = LBS.writeFile (dbPath db) $ A.encode $ all db
