3d8edca4d90627fdb2d948c6a61eec4792cec221
mizon <mizon9@gmail.com>
2012-02-25 02:18:13 +0900

    show message when server starts
    remove unused function

--- run-server.hs
+++ run-server.hs
@@ -1,38 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified RicEvents.Main as Main
+
 import qualified Network.Wai.Handler.Warp as Wp
-import qualified Network.Wai as W
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BC
-import qualified Data.ByteString.Lazy as LBS
-import qualified Network.HTTP.Types as HT
 import qualified System.Environment as E
 import qualified System.Console.GetOpt as O
 import Control.Applicative
-import Control.Monad.Trans
 
 main :: IO ()
 main = do
-  (opts, _, _) <- O.getOpt O.Permute optSpec <$> E.getArgs
-  Wp.run (getPort opts) app
+  p <- port
+  putStrLn $ "serve at " ++ show p
+  Wp.run p Main.waiApp
+  where
+    port = do
+      (opts, _, _) <- O.getOpt O.Permute optSpec <$> E.getArgs
+      return $ getPort opts
 
 getPort :: [ServerOption] -> Int
 getPort []               = undefined
 getPort (PortNumber n:_) = n
 getPort (_:os)           = getPort os
-
-app :: W.Application
-app req = do
-  bs <- liftIO $ BS.readFile "./hoge.txt"
-  return $ W.responseLBS HT.status200 headers
-         $ LBS.fromChunks
-           [ W.requestMethod req
-           , BC.pack $ show $ W.httpVersion req
-           , W.rawPathInfo req
-           , bs
-           ]
-  where
-    headers = [HT.headerContentType "text/plain"]
 
 data ServerOption
   = PortNumber Int
